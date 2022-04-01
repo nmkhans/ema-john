5a1f54e36f942ede37178693b2323421ad1727cc
Moin Khan <nurmoin05@gmail.com>
2022-04-01 21:37:19 +0600

    product component done

---
+++ src/components/Product/Product.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './Product.css';
+
+const Product = ({product}) => {
+
+    const {name, img, price, ratings, seller} = product;
+
+    return (
+        <div className="product">
+            <div className="inner__product">
+                <div className="product__content">
+                    <div className="product__card__top">
+                        <div className="product__card__feature">
+                            <img src={img} alt="" />
+                        </div>
+                        <div className="product__cart_decsription">
+                            <div className="product__name">
+                                <h3>{(name.length) > 20 ? name.slice(0, 20): name}</h3>
+                                <h4>Price: ${price} </h4>
+                            </div>
+                            <div className="product__info">
+                                <p>Manufacturer : {seller}</p>
+                                <p>Rating : {ratings} start</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="product__card__bottom">
+                        <button > Add to Cart</button> 
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Product;
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
 import './Products.css';
 
 const Products = () => {
+
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch('products.json')
+        .then(res => res.json())
+        .then(data => setProducts(data))
+    }, []);
+    const someProducts = products.slice(0,9);
+
     return (
         <div className="products">
             <div className="inner__products container">
                 <div className="products__content">
-                    
+                    {
+                        someProducts.map(product => <Product key={product.id} product={product} />)
+                    }
                 </div>
             </div>
         </div>
