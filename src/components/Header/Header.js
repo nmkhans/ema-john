c5b686a3a034f59703ae89c7e4a5692c9a7d8dfc
Moin Khan <nurmoin05@gmail.com>
2022-04-01 20:25:35 +0600

    project setup done

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="header__area">
+            <div className="inner__header__area container">
+                <div className="header__content">
+                    <h1>This is header</h1>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
