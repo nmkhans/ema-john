441354ab5b66fd2751af2fc71b067900654bb749
Moin Khan <nurmoin05@gmail.com>
2022-04-01 21:47:24 +0600

    shop page design done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,7 +4,24 @@ import './Cart.css';
 const Cart = () => {
     return (
         <div className="cart">
-            <h1>This is cart</h1>
+           <div className="cart__title">
+                <h3>Order Summary</h3>
+            </div>
+            <div className="cart__detail">
+                <div className="cart__specifications">
+                    <p>Selected Items: {6}</p>
+                    <p>Total Price: ${6}</p>
+                    <p>Total Shipping Charge: ${6}</p>
+                    <p>Tax: ${6}</p>
+                </div>
+                <div className="cart__grand__total">
+                    <h5>Grand Total: ${6}</h5>
+                </div>
+            </div>
+            <div className="cart__summery__button">
+                <button>Clear Cart</button>
+                <button>Review Order</button>
+            </div>
         </div>
     );
 };
